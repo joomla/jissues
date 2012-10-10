9ea331121efbdc4581b71f2347d685896288d782
Nikolai Plath <der.el.kuku@gmail.com>
2012-10-09 23:16:46 -0500

    Move com_users controllers from controllers/ to controller/

--- components/com_users/controller/user.php
+++ components/com_users/controller/user.php
@@ -21,84 +21,6 @@ require_once JPATH_COMPONENT.'/controller.php';
 class UsersControllerUser extends UsersController
 {
 	/**
-	 * Method to log in a user.
-	 *
-	 * @since	1.6
-	 */
-	public function login()
-	{
-		JSession::checkToken('post') or jexit(JText::_('JInvalid_Token'));
-
-		$app = JFactory::getApplication();
-
-		// Populate the data array:
-		$data = array();
-		$data['return'] = base64_decode($app->input->post->get('return', '', 'BASE64'));
-		$data['username'] = JRequest::getVar('username', '', 'method', 'username');
-		$data['password'] = JRequest::getString('password', '', 'post', JREQUEST_ALLOWRAW);
-
-		// Set the return URL if empty.
-		if (empty($data['return'])) {
-			$data['return'] = 'index.php?option=com_users&view=profile';
-		}
-
-		// Set the return URL in the user state to allow modification by plugins
-		$app->setUserState('users.login.form.return', $data['return']);
-
-		// Get the log in options.
-		$options = array();
-		$options['remember'] = $this->input->getBool('remember', false);
-		$options['return'] = $data['return'];
-
-		// Get the log in credentials.
-		$credentials = array();
-		$credentials['username'] = $data['username'];
-		$credentials['password'] = $data['password'];
-
-		// Perform the log in.
-		if (true === $app->login($credentials, $options)) {
-			// Success
-			$app->setUserState('users.login.form.data', array());
-			$app->redirect(JRoute::_($app->getUserState('users.login.form.return', 'index.php'), false));
-		} else {
-			// Login failed !
-			$data['remember'] = (int) $options['remember'];
-			$app->setUserState('users.login.form.data', $data);
-			$app->redirect(JRoute::_('index.php?option=com_users&view=login', false));
-		}
-	}
-
-	/**
-	 * Method to log out a user.
-	 *
-	 * @since	1.6
-	 */
-	public function logout()
-	{
-		JSession::checkToken('request') or jexit(JText::_('JInvalid_Token'));
-
-		$app = JFactory::getApplication();
-
-		// Perform the log in.
-		$error = $app->logout();
-
-		// Check if the log out succeeded.
-		if (!($error instanceof Exception)) {
-			// Get the return url from the request and validate that it is internal.
-			$return = JRequest::getVar('return', '', 'method', 'base64');
-			$return = base64_decode($return);
-			if (!JURI::isInternal($return)) {
-				$return = '';
-			}
-
-			// Redirect the user.
-			$app->redirect(JRoute::_($return, false));
-		} else {
-			$app->redirect(JRoute::_('index.php?option=com_users&view=login', false));
-		}
-	}
-
-	/**
 	 * Method to register a user.
 	 *
 	 * @since	1.6
@@ -114,10 +36,11 @@ class UsersControllerUser extends UsersController
 		$model  = $this->getModel('Registration', 'UsersModel');
 		$return	= $model->validate($data);
 
+		$app	= JFactory::getApplication();
+
 		// Check for errors.
 		if ($return === false) {
 			// Get the validation messages.
-			$app	= &JFactory::getApplication();
 			$errors	= $model->getErrors();
 
 			// Push up to three validation messages out to the user.
