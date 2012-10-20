32d3c7fed7cf10c5d1acaf462a2ab0c9790cb1b1
Michael Babker <mbabker@flbab.com>
2012-10-20 16:19:04 -0500

    Remove asset tracking from com_categories

--- administrator/components/com_categories/tables/category.php
+++ administrator/components/com_categories/tables/category.php
@@ -16,21 +16,119 @@ defined('_JEXEC') or die;
  * @subpackage  com_categories
  * @since       1.6
  */
-class CategoriesTableCategory extends JTableCategory
+class CategoriesTableCategory extends JTableNested
 {
 	/**
-	 * Method to delete a node and, optionally, its child nodes from the table.
+	 * Constructor
 	 *
-	 * @param   integer  $pk        The primary key of the node to delete.
-	 * @param   boolean  $children  True to delete child nodes, false to move them up a level.
+	 * @param   JDatabaseDriver  $db  Database driver object.
+	 *
+	 * @since   11.1
+	 */
+	public function __construct(JDatabaseDriver $db)
+	{
+		parent::__construct('#__categories', 'id', $db);
+
+		$this->access = (int) JFactory::getConfig()->get('access');
+	}
+
+	/**
+	 * Override check function
+	 *
+	 * @return  boolean
+	 *
+	 * @see     JTable::check
+	 * @since   11.1
+	 */
+	public function check()
+	{
+		// Check for a title.
+		if (trim($this->title) == '')
+		{
+			$this->setError(JText::_('JLIB_DATABASE_ERROR_MUSTCONTAIN_A_TITLE_CATEGORY'));
+			return false;
+		}
+		$this->alias = trim($this->alias);
+		if (empty($this->alias))
+		{
+			$this->alias = $this->title;
+		}
+
+		$this->alias = JApplication::stringURLSafe($this->alias);
+		if (trim(str_replace('-', '', $this->alias)) == '')
+		{
+			$this->alias = JFactory::getDate()->format('Y-m-d-H-i-s');
+		}
+
+		return true;
+	}
+
+	/**
+	 * Overloaded bind function.
+	 *
+	 * @param   array   $array   named array
+	 * @param   string  $ignore  An optional array or space separated list of properties
+	 *                           to ignore while binding.
+	 *
+	 * @return  mixed   Null if operation was satisfactory, otherwise returns an error
+	 *
+	 * @see     JTable::bind
+	 * @since   11.1
+	 */
+	public function bind($array, $ignore = '')
+	{
+		if (isset($array['params']) && is_array($array['params']))
+		{
+			$registry = new JRegistry;
+			$registry->loadArray($array['params']);
+			$array['params'] = (string) $registry;
+		}
+
+		if (isset($array['metadata']) && is_array($array['metadata']))
+		{
+			$registry = new JRegistry;
+			$registry->loadArray($array['metadata']);
+			$array['metadata'] = (string) $registry;
+		}
+
+		return parent::bind($array, $ignore);
+	}
+
+	/**
+	 * Overridden JTable::store to set created/modified and user id.
+	 *
+	 * @param   boolean  $updateNulls  True to update fields even if they are null.
 	 *
 	 * @return  boolean  True on success.
 	 *
-	 * @see     http://docs.joomla.org/JTableNested/delete
-	 * @since   2.5
+	 * @since   11.1
 	 */
-	public function delete($pk = null, $children = false)
+	public function store($updateNulls = false)
 	{
-		return parent::delete($pk, $children);
+		$date = JFactory::getDate();
+		$user = JFactory::getUser();
+
+		if ($this->id)
+		{
+			// Existing category
+			$this->modified_time = $date->toSql();
+			$this->modified_user_id = $user->get('id');
+		}
+		else
+		{
+			// New category
+			$this->created_time = $date->toSql();
+			$this->created_user_id = $user->get('id');
+		}
+		// Verify that the alias is unique
+		JTable::addIncludePath(__DIR__);
+		$table = JTable::getInstance('Category', 'CategoriesTable', array('dbo' => $this->getDbo()));
+		if ($table->load(array('alias' => $this->alias, 'parent_id' => $this->parent_id, 'extension' => $this->extension))
+			&& ($table->id != $this->id || $this->id == 0))
+		{
+			$this->setError(JText::_('JLIB_DATABASE_ERROR_CATEGORY_UNIQUE_ALIAS'));
+			return false;
+		}
+		return parent::store($updateNulls);
 	}
 }
