599081142f5a6238077da9f710b1846b7f10cdcb
Michael Babker <mbabker@flbab.com>
2013-01-05 14:59:19 -0600

    Sync up protostar

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -9,10 +9,21 @@
 
 defined('_JEXEC') or die;
 
+// Getting params from template
+$params = JFactory::getApplication()->getTemplate(true)->params;
+
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
 $this->language = $doc->language;
 $this->direction = $doc->direction;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->get('sitename');
 
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
@@ -21,20 +32,26 @@ JHtml::_('bootstrap.framework');
 $doc->addStyleSheet('templates/protostar/css/template.css');
 
 // Load optional rtl Bootstrap css and Bootstrap bugfixes
-JHtmlBootstrap::loadCss(false, $this->direction);
+JHtml::_('bootstrap.loadCss', false, $this->direction);
 
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<jdoc:include type="head" />
 	<!--[if lt IE 9]>
 		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
 	<![endif]-->
 </head>
 
-<body class="site fluid">
+<body class="site fluid <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '');
+?>">
+
 	<!-- Body -->
 	<div class="body">
 		<div class="container-fluid">
@@ -49,8 +66,12 @@ JHtmlBootstrap::loadCss(false, $this->direction);
 			<div class="row-fluid">
 				<div class="span7">
 					<ul class="nav nav-pills">
-						<li><a href="<?php echo JRoute::_('index.php?option=com_tracker'); ?>">Tracker</a></li>
-						<li><a href="<?php echo JRoute::_('index.php?option=com_users'); ?>">Users</a></li>
+						<li class="<?php echo (!$option || 'com_tracker' == $option) ? 'active' : '' ?>">
+							<a href="<?php echo JRoute::_('index.php?option=com_tracker'); ?>">Tracker</a>
+						</li>
+						<li class="<?php echo 'com_users' == $option ? 'active' : '' ?>">
+							<a href="<?php echo JRoute::_('index.php?option=com_users'); ?>">Users</a>
+						</li>
 					</ul>
 				</div>
 				<div class="span5">
@@ -76,7 +97,9 @@ JHtmlBootstrap::loadCss(false, $this->direction);
 		<div class="container-fluid">
 			<hr />
 			<p class="pull-right"><a href="#top" id="back-top"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
+			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
 		</div>
 	</div>
+	<jdoc:include type="modules" name="debug" style="none" />
 </body>
 </html>
