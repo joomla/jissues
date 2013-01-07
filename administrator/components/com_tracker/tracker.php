84a8792bed724868aea80f025b057acd57319a5a
Nikolai Plath <der.el.kuku@gmail.com>
2013-01-07 17:49:36 -0500

    Fix extra fields "general failure" admin part (Ref #69)

--- administrator/components/com_tracker/tracker.php
+++ administrator/components/com_tracker/tracker.php
@@ -24,9 +24,6 @@ TrackerHelper::addSubmenu(JFactory::getApplication()->input->get('view'));
 
 JHTML::_('addIncludePath', JPATH_LIBRARIES . '/tracker/html');
 
-// "load" a class with the _() function so we can call it directly later.
-JHtml::_('projects.select', 'x', 'y');
-
 $controller = JControllerLegacy::getInstance('Tracker');
 $controller->execute(JFactory::getApplication()->input->get('task'));
 $controller->redirect();
--- administrator/components/com_tracker/views/tracker/tmpl/default_project.php
+++ administrator/components/com_tracker/views/tracker/tmpl/default_project.php
@@ -8,26 +8,46 @@
  */
 
 defined('_JEXEC') or die;
+
+$baseLinkAdd = 'index.php?option=com_categories&view=category&layout=edit&task=category.add&extension=com_tracker'. '.' . $this->project->id;
+$buttonStyles = array('class' => 'btn btn-small btn-success');
+
 ?>
 
 <div class="row">
-    <div class="span12">
-        <h2>Categories</h2>
-		<?= $this->lists->get('categories') ? : 'Use global' ?>
-    </div>
+	<div class="span4">
+		<h2>Categories</h2>
+		<?= JHtml::link($baseLinkAdd . '.categories', 'Add a Category', $buttonStyles) ?>
+		<div class="well well-small">
+			<?= JHtmlCustomfields::getItems('categories', $this->project->id) ? JHtmlProjects::listing('categories', $this->project->id) : 'Use global' ?>
+		</div>
+
+	</div>
 </div>
 <div class="row">
-    <div class="span4">
-        <h2>Textfields</h2>
-		<?= $this->lists->get('textfields') ? : 'Use global' ?>
-    </div>
-    <div class="span4">
-        <h2>Selectlists</h2>
-		<?= $this->lists->get('fields') ? : 'Use global' ?>
-    </div>
-    <div class="span4">
-        <h2>Checkboxes</h2>
-		<?= $this->lists->get('checkboxes') ? : 'Use global' ?>
-    </div>
+	<div class="span4">
+		<h2>Textfields</h2>
+		<?= JHtml::link($baseLinkAdd . '.textfields', 'Add a Textfield', $buttonStyles) ?>
+		<div class="well well-small">
+			<?= JHtmlCustomfields::getItems('textfields', $this->project->id) ? JHtmlProjects::listing('textfields', $this->project->id) : 'Use global' ?>
+		</div>
+
+	</div>
+	<div class="span4">
+		<h2>Selectlists</h2>
+		<?= JHtml::link($baseLinkAdd . '.fields', 'Add a Selectlist', $buttonStyles) ?>
+		<div class="well well-small">
+			<?= JHtmlCustomfields::getItems('fields', $this->project->id) ? JHtmlProjects::listing('fields', $this->project->id) : 'Use global' ?>
+		</div>
+
+	</div>
+	<div class="span4">
+		<h2>Checkboxes</h2>
+		<?= JHtml::link($baseLinkAdd . '.checkboxes', 'Add a Checkbox', $buttonStyles) ?>
+		<div class="well well-small">
+			<?= JHtmlCustomfields::getItems('checkboxes', $this->project->id) ? JHtmlProjects::listing('checkboxes', $this->project->id) : 'Use global' ?>
+		</div>
+
+	</div>
 </div>
 
