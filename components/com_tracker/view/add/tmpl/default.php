e63329551e2b0709ebe87bce23a83b986fda0362
Nikolai Plath <der.el.kuku@gmail.com>
2013-01-08 15:15:14 -0500

    Move the text for new issue descriptions to a proper template file.

--- components/com_tracker/view/add/tmpl/default.php
+++ components/com_tracker/view/add/tmpl/default.php
@@ -13,33 +13,7 @@ defined('_JEXEC') or die;
 
 JHtmlBootstrap::tooltip();
 
-// @todo move the "template" to ... a template ¿
-$template = "
-Please enter a description of the issue
-
-## Test instructions
-
-Please add some instructions on how to reproduce the issue
-
-Example:
-
-* Install Joomla! with sample data
-* Browse to the backend and click...
-
-## Test code
-You may add some code if needed
-
-```php
-echo 'my issue';
-```
-
-## Screenshots
-You may upload screenshots or reference external images here.
-
-Example:
-
-![Joomla! World Conference](http://conference.joomla.org/images/banners/general/728x90.jpg)
-";
+$template = file_get_contents(__DIR__ . '/new_issue_template.md');
 
 ?>
 <h1>Add a new Issue</h1>
