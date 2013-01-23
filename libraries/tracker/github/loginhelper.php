a5edddd05946b2fe8d7d54b691a69f2aeacff145
Nikolai Plath <der.el.kuku@gmail.com>
2013-01-23 12:00:29 -0500

    Some refactoring of the ugly loginhelper class - @todo refactorme more =;)

--- libraries/tracker/github/loginhelper.php
+++ libraries/tracker/github/loginhelper.php
@@ -16,16 +16,8 @@ defined('_JEXEC') or die;
  * @subpackage  GitHub
  * @since       1.0
  */
-abstract class JGithubLoginhelper extends JGithub
+final class JGithubLoginhelper
 {
-	public static $baseUri = 'https://api.github.com';
-
-	public static $headers = array('Accept' => 'application/json');
-
-	protected static $oAuthAccessToken = '';
-
-	protected static $ghUserName;
-
 	/**
 	 * Login.
 	 *
@@ -33,24 +25,35 @@ abstract class JGithubLoginhelper extends JGithub
 	 *
 	 * @throws DomainException
 	 *
-	 * @return bool
+	 * @return JGithubUser
 	 */
 	public static function login()
 	{
-		if (JFactory::getSession()->get('gh_oauth_access_token'))
+		if (JFactory::getUser()->id)
 		{
 			// Already logged in
-			return true;
+
+			if (JFactory::getSession()->get('gh_oauth_access_token'))
+			{
+				// And the token exists
+
+				return true;
+			}
+			else
+			{
+				// But somehow we've lost our token :(
+
+				static::logout();
+
+				// Proceed with login...
+			}
 		}
 
 		$input = JFactory::getApplication()->input;
 
 		try
 		{
-			/*
-			 * 1)
-			 * Verify the response (code)
-			 */
+			// Verify the response (code)
 
 			$error = $input->get('error');
 
@@ -59,7 +62,7 @@ abstract class JGithubLoginhelper extends JGithub
 				switch ($error)
 				{
 					case 'access_denied' :
-						throw new DomainException('Authorization failed (1)');
+						throw new DomainException('Authorization failed.');
 						break;
 
 					default :
@@ -76,178 +79,116 @@ abstract class JGithubLoginhelper extends JGithub
 				throw new DomainException('No code received from GitHub :(');
 			}
 
-			/*
-			 * 2)
-			 * Obtain the access token
-			 */
+			// Obtain the access token
 
-			$config = JFactory::getConfig();
-
-			$http = new JHttp;
-
-			$uri = 'https://github.com/login/oauth/access_token';
-
-			$data = array(
-				'client_id'     => $config->get('github_client_id'),
-				'client_secret' => $config->get('github_client_secret'),
-				'code'          => $code
-			);
-
-			$response = $http->post($uri, $data, static::$headers);
-
-			if (200 != $response->code)
-			{
-				if (JDEBUG)
-				{
-					var_dump($response);
-				}
-
-				throw new DomainException('Invalid response from GitHub (2) :(');
-			}
-
-			$body = json_decode($response->body);
-
-			if (isset($body->error))
-			{
-				switch ($body->error)
-				{
-					case 'bad_verification_code' :
-						throw new DomainException('bad verification code');
-						break;
-
-					default :
-						throw new DomainException('Unknown (2) ' . $body->error);
-						break;
-				}
-			}
-
-			if (!isset($body->access_token))
-			{
-				throw new DomainException('Can not retrieve the access token');
-			}
-
-			$access_token = $body->access_token;
-
-
-			/*
-			 * 3)
-			 * Get the current logged in user
-			 */
-
-			$base = 'https://api.github.com';
-			$response = $http->get($base . '/user?access_token=' . $access_token);
-
-			if (200 != $response->code)
-			{
-				if (JDEBUG)
-				{
-					var_dump($response);
-				}
-
-				throw new DomainException('Invalid response from GitHub (3)');
-			}
-
-			$body = json_decode($response->body);
+			$access_token = static::requestToken($code);
 
 			// Store the token into the session
 			JFactory::getSession()->set('gh_oauth_access_token', $access_token);
-			JFactory::getSession()->set('gh_user_name', $body->login);
 
-			self::$oAuthAccessToken = $access_token;
-			self::$ghUserName = $body->login;
+			// Get the current logged in GitHub user
+
+			$options = new JRegistry;
+			$options->set('gh.token', $access_token);
+
+			$gitHub = new JGithub($options);
+
+			$gitHubUser = $gitHub->users->getAuthenticatedUser();
+
+			// All good.
+
+			return new JGithubUser($gitHubUser);
 		}
 		catch (DomainException $e)
 		{
 			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
-
-			static::clearCredentials();
-
-			return false;
 		}
 
-		return true;
+		// Something went wrong...
+
+		static::logout();
+
+		return false;
 	}
 
 	/**
-	 * Add a comment.
-	 *
-	 * @param   JTrackerProject  $project      The project.
-	 * @param   integer          $issueNumber  The issue number.
-	 * @param   string           $comment      The comment.
-	 *
-	 * @throws RuntimeException
+	 * Clear the GitHub oAuth token.
 	 *
 	 * @return void
 	 */
-	public static function comment(JTrackerProject $project, $issueNumber, $comment)
+	public static function logout()
 	{
-		if (!static::getToken())
-		{
-			throw new RuntimeException('Missing gh token');
-		}
+		JFactory::getSession()->set('gh_oauth_access_token', null);
+
+		JFactory::getApplication()->logout();
+	}
+
+	/**
+	 * Get the GitHub oAuth token.
+	 *
+	 * @return string
+	 */
+	public static function getToken()
+	{
+		return JFactory::getSession()->get('gh_oauth_access_token');
+	}
+
+	/**
+	 * Request an oAuth token from GitHub.
+	 *
+	 * @param   string  $code  The code obtained form GitHub on the previous step.
+	 *
+	 * @throws DomainException
+	 * @return mixed
+	 */
+	private static function requestToken($code)
+	{
+		$config = JFactory::getConfig();
 
 		$http = new JHttp;
 
-		$owner = $project->gh_user;
-		$repo  = $project->gh_project;
+		$uri = 'https://github.com/login/oauth/access_token';
 
-		$url = static::$baseUri . '/repos/' . $owner . '/' . $repo . '/issues/' . $issueNumber
-			. '/comments?access_token=' . static::$oAuthAccessToken;
-
-		$comment .= sprintf(
-			'<hr />Please blame the <a href="%1$s">%2$s Application</a> for transmitting this comment.',
-			'https://github.com/JTracker/jissues', 'JTracker'
+		$data = array(
+			'client_id'     => $config->get('github_client_id'),
+			'client_secret' => $config->get('github_client_secret'),
+			'code'          => $code
 		);
 
-		$data = json_encode(array('body' => $comment));
+		$response = $http->post($uri, $data, array('Accept' => 'application/json'));
 
-		$headers = array('Accept' => 'application/json');
-
-		$response = $http->post($url, $data, $headers);
-
-		$body = json_decode($response->body);
-
-		if (201 != $response->code)
+		if (200 != $response->code)
 		{
-			JFactory::getApplication()->enqueueMessage('GitHub error: ' . $body->message, 'error');
-
 			if (JDEBUG)
 			{
 				var_dump($response);
 			}
 
-			static::clearCredentials();
-
-			return;
+			throw new DomainException('Invalid response from GitHub (2) :(');
 		}
 
-		JFactory::getApplication()->enqueueMessage('Your comment has been added');
-	}
+		$body = json_decode($response->body);
 
-	public static function getToken()
-	{
-		if('' == static::$oAuthAccessToken)
+		if (isset($body->error))
 		{
-			static::$oAuthAccessToken = JFactory::getSession()->get('gh_oauth_access_token');
+			switch ($body->error)
+			{
+				case 'bad_verification_code' :
+					throw new DomainException('bad verification code');
+					break;
+
+				default :
+					throw new DomainException('Unknown (2) ' . $body->error);
+					break;
+			}
 		}
 
-		return static::$oAuthAccessToken;
-	}
+		if (!isset($body->access_token))
+		{
+			throw new DomainException('Can not retrieve the access token');
+		}
 
-	public static function getGhUsername()
-	{
-		return static::$ghUserName;
-	}
-
-	/**
-	 * Clear the oAuth credentials.
-	 *
-	 * @return void
-	 */
-	public static function clearCredentials()
-	{
-		JFactory::getSession()->set('gh_oauth_access_token', null);
-		JFactory::getSession()->set('gh_user_name', null);
+		return $body->access_token;
 	}
 
 }
