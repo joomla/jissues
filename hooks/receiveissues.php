39a7bcd51e3129eb25ab1c58b4ae34d37a934bc7
Michael Babker <mbabker@flbab.com>
2013-02-01 16:17:43 -0600

    Killing some duplicate lines

--- hooks/receiveissues.php
+++ hooks/receiveissues.php
@@ -24,12 +24,21 @@ require_once __DIR__ . '/bootstrap.php';
 final class TrackerReceiveIssues extends JApplicationHooks
 {
 	/**
-	 * The project information of the project whose data has been received
+	 * Class constructor.
 	 *
-	 * @var    object
-	 * @since  1.0
+	 * @since   1.0
 	 */
-	protected $project;
+	public function __construct()
+	{
+		// Set the type of hook
+		$this->hookType = 'issues';
+
+		// Run the parent constructor
+		parent::__construct();
+
+		// Get the project data
+		$this->getProjectData();
+	}
 
 	/**
 	 * Method to run the application routines.
@@ -40,22 +49,11 @@ final class TrackerReceiveIssues extends JApplicationHooks
 	 */
 	public function doExecute()
 	{
-		// Initialize the logger
-		$options['format']    = '{DATE}\t{TIME}\t{LEVEL}\t{CODE}\t{MESSAGE}';
-		$options['text_file'] = 'github_issues.php';
-		JLog::addLogger($options);
-
 		// Initialize the database
 		$query = $this->db->getQuery(true);
 
-		// Get the data directly from the $_POST superglobal.  I've yet to make this work with JInput.
-		$data = $_POST['payload'];
-
-		// Decode it
-		$data = json_decode($data);
-
 		// Get the issue ID
-		$githubID = $data->issue->number;
+		$githubID = $this->hookData->issue->number;
 
 		// Check to see if the issue is already in the database
 		$query->select($this->db->quoteName('id'));
@@ -73,41 +71,17 @@ final class TrackerReceiveIssues extends JApplicationHooks
 			$this->close();
 		}
 
-		// Get the info for the project on our tracker
-		$query->clear();
-		$query->select('*');
-		$query->from($this->db->quoteName('#__tracker_projects'));
-		$query->where($this->db->quoteName('gh_project') . ' = ' . $this->db->quote($data->repository->name));
-		$this->db->setQuery($query);
-
-		try
-		{
-			$this->project = $this->db->loadObject();
-		}
-		catch (RuntimeException $e)
-		{
-			JLog::add(sprintf('Error retrieving the project ID for GitHub repo %s in the database: %s', $data->repository->name, $e->getMessage()), JLog::INFO);
-			$this->close();
-		}
-
-		// Make sure we have a valid project ID
-		if (!$this->project->project_id)
-		{
-			JLog::add(sprintf('A project does not exist for the %s GitHub repo in the database, cannot add data for it.', $data->repository->name), JLog::INFO);
-			$this->close();
-		}
-
 		// Instantiate the JTable instance
 		$table = JTable::getInstance('Issue');
 
 		// If the item is already in the databse, update it; else, insert it
 		if ($issueID)
 		{
-			$this->updateData($issueID, $data);
+			$this->updateData($issueID);
 		}
 		else
 		{
-			$this->insertData($table, $data);
+			$this->insertData($table);
 		}
 	}
 
@@ -115,16 +89,15 @@ final class TrackerReceiveIssues extends JApplicationHooks
 	 * Method to insert data for an issue from GitHub
 	 *
 	 * @param   JTableIssue  $table  Issue table instance
-	 * @param   object       $data   The hook data
 	 *
 	 * @return  boolean  True on success
 	 *
 	 * @since   1.0
 	 */
-	protected function insertData(JTableIssue $table, $data)
+	protected function insertData(JTableIssue $table)
 	{
 		// Figure out the state based on the action
-		$action = $data->action;
+		$action = $this->hookData->action;
 
 		switch ($action)
 		{
@@ -140,13 +113,10 @@ final class TrackerReceiveIssues extends JApplicationHooks
 
 		}
 
-		// Get a JGithub instance to parse the body through their parser
-		$github = new JGithub;
-
 		// Try to render the description with GitHub markdown
 		try
 		{
-			$issue = $github->markdown->render($data->issue->body, 'gfm', $this->project->gh_user . '/' . $this->project->gh_project);
+			$issue = $this->github->markdown->render($this->hookData->issue->body, 'gfm', $this->project->gh_user . '/' . $this->project->gh_project);
 		}
 		catch (DomainException $e)
 		{
@@ -154,37 +124,37 @@ final class TrackerReceiveIssues extends JApplicationHooks
 			$this->close();
 		}
 
-		$table->gh_id       = $data->issue->number;
-		$table->title       = $data->issue->title;
+		$table->gh_id       = $this->hookData->issue->number;
+		$table->title       = $this->hookData->issue->title;
 		$table->description = $issue;
 		$table->status      = $status;
-		$table->opened      = JFactory::getDate($data->issue->created_at)->toSql();
-		$table->modified    = JFactory::getDate($data->issue->updated_at)->toSql();
+		$table->opened      = JFactory::getDate($this->hookData->issue->created_at)->toSql();
+		$table->modified    = JFactory::getDate($this->hookData->issue->updated_at)->toSql();
 		$table->project_id  = $this->project->project_id;
 
 		// Add the diff URL if this is a pull request
-		if ($data->issue->pull_request->diff_url)
+		if ($this->hookData->issue->pull_request->diff_url)
 		{
-			$table->patch_url = $data->issue->pull_request->diff_url;
+			$table->patch_url = $this->hookData->issue->pull_request->diff_url;
 		}
 
 		// Add the closed date if the status is closed
-		if ($data->issue->closed_at)
+		if ($this->hookData->issue->closed_at)
 		{
-			$table->closed_date = JFactory::getDate($data->issue->closed_at)->toSql();
+			$table->closed_date = JFactory::getDate($this->hookData->issue->closed_at)->toSql();
 		}
 
 		// If the title has a [# in it, assume it's a Joomlacode Tracker ID
 		// TODO - Would be better suited as a regex probably
-		if (strpos($data->issue->title, '[#') !== false)
+		if (strpos($this->hookData->issue->title, '[#') !== false)
 		{
-			$pos = strpos($data->issue->title, '[#') + 2;
-			$table->jc_id = substr($data->issue->title, $pos, 5);
+			$pos = strpos($this->hookData->issue->title, '[#') + 2;
+			$table->jc_id = substr($this->hookData->issue->title, $pos, 5);
 		}
 
 		if (!$table->store())
 		{
-			JLog::add(sprintf('Error storing new item %s in the database: %s', $data->issue->number, $table->getError()), JLog::INFO);
+			JLog::add(sprintf('Error storing new item %s in the database: %s', $this->hookData->issue->number, $table->getError()), JLog::INFO);
 			$this->close();
 		}
 
@@ -192,7 +162,7 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		$query->clear();
 		$query->select('id');
 		$query->from($this->db->quoteName('#__issues'));
-		$query->where($this->db->quoteName('gh_id') . ' = ' . (int) $data->issue->number);
+		$query->where($this->db->quoteName('gh_id') . ' = ' . (int) $this->hookData->issue->number);
 		$this->db->setQuery($query);
 
 		try
@@ -201,14 +171,14 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		}
 		catch (RuntimeException $e)
 		{
-			JLog::add(sprintf('Error retrieving ID for GitHub issue %s in the database: %s', $data->issue->number, $e->getMessage()), JLog::INFO);
+			JLog::add(sprintf('Error retrieving ID for GitHub issue %s in the database: %s', $this->hookData->issue->number, $e->getMessage()), JLog::INFO);
 			$this->close();
 		}
 
 		// Add an open record to the activity table
 		$activity = new JTableActivity($this->db);
 		$activity->issue_id = (int) $issueID;
-		$activity->user     = $data->issue->user->login;
+		$activity->user     = $this->hookData->issue->user->login;
 		$activity->event    = 'open';
 		$activity->created  = $table->opened;
 
@@ -223,7 +193,7 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		{
 			$activity = new JTableActivity($this->db);
 			$activity->issue_id = (int) $issueID;
-			$activity->user     = $data->issue->user->login;
+			$activity->user     = $this->hookData->issue->user->login;
 			$activity->event    = 'reopen';
 			$activity->created  = $table->modified;
 
@@ -235,11 +205,11 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		}
 
 		// Add a close record to the activity table if the status is closed
-		if ($data->issue->closed_at)
+		if ($this->hookData->issue->closed_at)
 		{
 			$activity = new JTableActivity($this->db);
 			$activity->issue_id = (int) $issueID;
-			$activity->user     = $data->issue->user->login;
+			$activity->user     = $this->hookData->issue->user->login;
 			$activity->event    = 'close';
 			$activity->created  = $table->closed_date;
 
@@ -251,7 +221,7 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		}
 
 		// Store was successful, update status
-		JLog::add(sprintf('Added GitHub issue %s to the tracker.', $data->issue->number), JLog::INFO);
+		JLog::add(sprintf('Added GitHub issue %s to the tracker.', $this->hookData->issue->number), JLog::INFO);
 
 		return true;
 	}
@@ -260,16 +230,15 @@ final class TrackerReceiveIssues extends JApplicationHooks
 	 * Method to update data for an issue from GitHub
 	 *
 	 * @param   integer  $issueID  Issue ID in the database
-	 * @param   object   $data     The hook data
 	 *
 	 * @return  boolean  True on success
 	 *
 	 * @since   1.0
 	 */
-	protected function updateData($issueID, $data)
+	protected function updateData($issueID)
 	{
 		// Figure out the state based on the action
-		$action = $data->action;
+		$action = $this->hookData->action;
 
 		switch ($action)
 		{
@@ -285,13 +254,10 @@ final class TrackerReceiveIssues extends JApplicationHooks
 
 		}
 
-		// Get a JGithub instance to parse the body through their parser
-		$github = new JGithub;
-
 		// Try to render the description with GitHub markdown
 		try
 		{
-			$issue = $github->markdown->render($data->issue->body, 'gfm', $this->project->gh_user . '/' . $this->project->gh_project);
+			$issue = $this->github->markdown->render($this->hookData->issue->body, 'gfm', $this->project->gh_user . '/' . $this->project->gh_project);
 		}
 		catch (DomainException $e)
 		{
@@ -302,16 +268,16 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		// Only update fields that may have changed, there's no API endpoint to show that so make some guesses
 		$query = $this->db->getQuery(true);
 		$query->update($this->db->quoteName('#__issues'));
-		$query->set($this->db->quoteName('title') . ' = ' . $this->db->quote($data->issue->title));
+		$query->set($this->db->quoteName('title') . ' = ' . $this->db->quote($this->hookData->issue->title));
 		$query->set($this->db->quoteName('description') . ' = ' . $this->db->quote($issue));
 		$query->set($this->db->quoteName('status') . ' = ' . $status);
-		$query->set($this->db->quoteName('modified') . ' = ' . $this->db->quote(JFactory::getDate($data->issue->updated_at)->toSql()));
+		$query->set($this->db->quoteName('modified') . ' = ' . $this->db->quote(JFactory::getDate($this->hookData->issue->updated_at)->toSql()));
 		$query->where($this->db->quoteName('id') . ' = ' . $issueID);
 
 		// Add the closed date if the status is closed
-		if ($data->issue->closed_at)
+		if ($this->hookData->issue->closed_at)
 		{
-			$query->set($this->db->quoteName('closed_date') . ' = ' . $this->db->quote(JFactory::getDate($data->issue->closed_at)->toSql()));
+			$query->set($this->db->quoteName('closed_date') . ' = ' . $this->db->quote(JFactory::getDate($this->hookData->issue->closed_at)->toSql()));
 		}
 
 		try
@@ -330,9 +296,9 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		{
 			$activity = new JTableActivity($this->db);
 			$activity->issue_id = $issueID;
-			$activity->user     = $data->issue->user->login;
+			$activity->user     = $this->hookData->issue->user->login;
 			$activity->event    = 'reopen';
-			$activity->created  = JFactory::getDate($data->issue->updated_at)->toSql();
+			$activity->created  = JFactory::getDate($this->hookData->issue->updated_at)->toSql();
 
 			if (!$activity->store())
 			{
@@ -342,13 +308,13 @@ final class TrackerReceiveIssues extends JApplicationHooks
 		}
 
 		// Add a close record to the activity table if the status is closed
-		if ($data->issue->closed_at)
+		if ($this->hookData->issue->closed_at)
 		{
 			$activity = new JTableActivity($this->db);
 			$activity->issue_id = $issueID;
-			$activity->user     = $data->issue->user->login;
+			$activity->user     = $this->hookData->issue->user->login;
 			$activity->event    = 'close';
-			$activity->created  = JFactory::getDate($data->issue->closed_at)->toSql();
+			$activity->created  = JFactory::getDate($this->hookData->issue->closed_at)->toSql();
 
 			if (!$activity->store())
 			{
