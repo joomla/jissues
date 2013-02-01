4ceade090cbfb01c3f55c2e0511bf2a5b80a6227
Michael Babker <mbabker@flbab.com>
2013-01-31 20:19:41 -0600

    Add a new hook IP

--- hooks/receiveissues.php
+++ hooks/receiveissues.php
@@ -9,7 +9,7 @@
  */
 
 // Check the request is coming from GitHub
-$validIps = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
+$validIps = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
 if (!in_array($_SERVER['REMOTE_ADDR'], $validIps))
 {
 	die("You don't belong here!");
