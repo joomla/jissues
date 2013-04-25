910089cdab9fbb930e498cf7d1cc7be8bb818708
Nikolai Plath <der.el.kuku@gmail.com>
2013-04-25 18:13:18 -0500

    Add a projects model

---
+++ src/Joomla/Tracker/Components/Tracker/Model/ProjectsModel.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     JTracker\Components\Tracker
+ *
+ * @copyright   Copyright (C) 2012 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Tracker\Components\Tracker\Model;
+
+use Joomla\Database\DatabaseQuery;
+use Joomla\Tracker\Model\AbstractTrackerListModel;
+
+/**
+ * Model to get data for the projects list view
+ *
+ * @package  JTracker\Components\Tracker
+ * @since    1.0
+ */
+class ProjectsModel extends AbstractTrackerListModel
+{
+	/**
+	 * Method to get a DatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return  DatabaseQuery   A DatabaseQuery object to retrieve the data set.
+	 *
+	 * @since   1.0
+	 */
+	protected function getListQuery()
+	{
+		$db    = $this->getDb();
+		$query = $db->getQuery(true);
+
+		$query->select('a.*');
+		$query->from($db->quoteName('#__tracker_projects', 'a'));
+
+		return $query;
+	}
+}
