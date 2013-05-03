5e3882338e4a64f33dd758f8f2954f54de9be2e2
Nikolai Plath <der.el.kuku@gmail.com>
2013-05-03 15:30:04 -0500

    Add User::load() by id

--- src/Joomla/Tracker/Authentication/User.php
+++ src/Joomla/Tracker/Authentication/User.php
@@ -6,6 +6,9 @@
 
 namespace Joomla\Tracker\Authentication;
 
+use Joomla\Factory;
+use Joomla\Tracker\Authentication\Database\TableUsers;
+
 abstract class User
 {
 	public $id = 0;
@@ -15,6 +18,8 @@ abstract class User
 	public $name = '';
 
 	public $email = '';
+
+	public $registerDate = '';
 
 	/**
 	 * Constructor activating the default information of the language
@@ -28,58 +33,52 @@ abstract class User
 		// Load the user if it exists
 		if ($identifier)
 		{
-			throw new \UnexpectedValueException('Load by id not supported yet');
-
-			//$this->load($identifier);
+			$this->load($identifier);
 		}
 	}
 
 	/**
 	 * Method to load a JUser object by user id number
 	 *
-	 * @param   mixed  $id  The user id of the user to load
+	 * @param   mixed $id  The user id of the user to load
 	 *
-	 * @return  boolean  True on success
+	 * @throws \RuntimeException
+	 * @since   1.0
 	 *
-	 * @since   11.1
+	 * @return $this
 	 */
-	public function load($id)
+	protected function load($id)
 	{
 		// Create the user table object
-		$table = $this->getTable();
+		//$table = $this->getTable();
+		$table = new TableUsers(Factory::$application->getDatabase());
 
 		// Load the JUserModel object based on the user id or throw a warning.
 		if (!$table->load($id))
 		{
-			// Reset to guest user
-			$this->guest = 1;
-
-			JLog::add(JText::sprintf('JLIB_USER_ERROR_UNABLE_TO_LOAD_USER', $id), JLog::WARNING, 'jerror');
-
-			return false;
+			throw new \RuntimeException('Unable to load the user with id: ' . $id);
 		}
 
 		/*
 		 * Set the user parameters using the default XML file.  We might want to
 		 * extend this in the future to allow for the ability to have custom
 		 * user parameters, but for right now we'll leave it how it is.
+		 *
+		 * EDIT (elkuku): right now it's disabled =;)
 		 */
 
-		$this->_params->loadString($table->params);
+		// $this->_params->loadString($table->params);
 
 		// Assuming all is well at this point let's bind the data
-		$this->setProperties($table->getProperties());
 
-		// The user is no longer a guest
-		if ($this->id != 0)
+		foreach ($table->getFields() as $k => $v)
 		{
-			$this->guest = 0;
-		}
-		else
-		{
-			$this->guest = 1;
+			if (isset($this->$k))
+			{
+				$this->$k = $table->$k;
+			}
 		}
 
-		return true;
+		return $this;
 	}
 }
