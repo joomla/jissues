6ef208a2c9ae83bf28313008f5ce59dad2d0de8a
Michael Babker <mbabker@flbab.com>
2013-05-15 19:32:19 -0500

    Adding to Nikolai's last push

---
+++ src/Joomla/Tracker/Model/TrackerDefaultModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     JTracker\Components\Tracker
+ *
+ * @copyright   Copyright (C) 2012 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Tracker\Model;
+
+use Joomla\Tracker\Model\AbstractTrackerDatabaseModel;
+
+/**
+ * Default model class for the Tracker application.
+ *
+ * @package  JTracker\Components\Tracker
+ * @since    1.0
+ */
+class TrackerDefaultModel extends AbstractTrackerDatabaseModel
+{
+}
