b4111f6c928afab9cc29f8f77904490d70097c44
Nikolai Plath <der.el.kuku@gmail.com>
2013-06-18 20:11:37 -0500

    Add a CLI "make" command

---
+++ cli/CliApp/Command/Make/Docu.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2013 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace CliApp\Command\Make;
+
+use App\Text\Table\ArticlesTable;
+use CliApp\Application\TrackerApplication;
+
+/**
+ * Class for retrieving issues from GitHub for selected projects
+ *
+ * @since  1.0
+ */
+class Docu extends Make
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   TrackerApplication  $application  The application object.
+	 *
+	 * @since   1.0
+	 */
+	public function __construct(TrackerApplication $application)
+	{
+		$this->application = $application;
+		$this->description = 'Compile documentation using GitHub markdown.';
+		$this->usePBar     = $this->application->get('cli-application.progress-bar');
+
+		if ($this->application->input->get('noprogress'))
+		{
+			$this->usePBar = false;
+		}
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function execute()
+	{
+		$this->application->outputTitle('Make Documentation');
+
+		$this->github = $this->application->getGitHub();
+
+		$this->application->displayGitHubRateLimit();
+
+		$db         = $this->application->getDatabase();
+		$docuBase   = JPATH_ROOT . '/Documentation';
+		$pagePrefix = 'dox-';
+		$files      = array();
+
+		/* @type \DirectoryIterator $f */
+		foreach (new \DirectoryIterator($docuBase) as $f)
+		{
+			if ($f->isDot())
+			{
+				continue;
+			}
+
+			$files[] = $f->getFilename();
+		}
+
+		$progressBar = $this->application->getProgressBar(count($files));
+
+		$this->usePBar ? $this->out() : null;
+
+		$this
+			->out('Compiling documentation in: ' . $docuBase)
+			->out('Page prefix: ' . $pagePrefix)
+			->out();
+
+		$table = new ArticlesTable($db);
+
+		foreach ($files as $i => $file)
+		{
+			// @todo enough for URIs ?
+			$page = $pagePrefix . substr($file, 0, strrpos($file, '.'));
+
+			$this->debugOut('Compiling: ' . $page);
+
+			$table->reset();
+			$table->{$table->getKeyName()} = null;
+
+			try
+			{
+				$table->load(array('alias' => $page));
+			}
+			catch (\RuntimeException $e)
+			{
+				// New item
+			}
+
+			$table->alias   = $page;
+			$table->text_md = file_get_contents($docuBase . '/' . $file);
+
+			$table->store();
+
+			$this->usePBar
+				? $progressBar->update($i + 1)
+				: $this->out('.', false);
+		}
+
+		$this->out()
+			->out('Finished =;)');
+	}
+}
---
+++ cli/CliApp/Command/Make/Make.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2013 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace CliApp\Command\Make;
+
+use CliApp\Application\TrackerApplication;
+
+use CliApp\Command\TrackerCommand;
+use CliApp\Command\TrackerCommandOption;
+
+/**
+ * Class for retrieving issues from GitHub for selected projects
+ *
+ * @since  1.0
+ */
+class Make extends TrackerCommand
+{
+	/**
+	 * Joomla! Github object
+	 *
+	 * @var    \Joomla\Github\Github
+	 * @since  1.0
+	 */
+	protected $github;
+
+	/**
+	 * Use the progress bar.
+	 *
+	 * @var    boolean
+	 * @since  1.0
+	 */
+	protected $usePBar;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   TrackerApplication  $application  The application object.
+	 *
+	 * @since   1.0
+	 */
+	public function __construct(TrackerApplication $application)
+	{
+		parent::__construct($application);
+
+		$this->description = 'The make engine.';
+
+		$this->addOption(
+			new TrackerCommandOption(
+				'noprogress', '',
+				'Don\'t use a progress bar.'
+			)
+		);
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function execute()
+	{
+		$this->application->outputTitle('Make');
+
+		$this->out('<error>                                    </error>');
+		$this->out('<error>  Please use one of the following:  </error>');
+		$this->out('<error>  make docu                         </error>');
+		$this->out('<error>                                    </error>');
+	}
+}
