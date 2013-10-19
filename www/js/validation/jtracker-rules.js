59e1d33db4e63d44f63cd8408a82c72992cc2af2
Dmitry Rekun <bzzjuka@gmail.com>
2013-10-19 21:42:08 +0300

    Implement client side form validation

---
+++ www/js/validation/jtracker-rules.js
@@ -0,0 +1,25 @@
+/**
+ * @copyright  Copyright (C) 2012 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+$.validator.addMethod("allowedchars", function(value, element) {
+	var regex = /^[a-z0-9\-.,()\[\]'"+_@&$#%\s]+$/i;
+	return this.optional(element) || regex.test(value);
+}, "This character is not allowed here.");
+
+$.validator.addClassRules({
+	validateTitle: {
+		allowedchars: true,
+		required: true,
+		minlength: 15,
+		maxlength: 50
+	},
+	validateBuild: {
+		required: true,
+		maxlength: 40
+	},
+	validateDescription: {
+		required: true
+	}
+});
