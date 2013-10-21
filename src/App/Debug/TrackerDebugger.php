1cb77cbb98c1021e8d652c23ead8cb916131967d
Nikolai Plath <der.el.kuku@gmail.com>
2013-10-20 20:30:31 -0500

    Improve the debug toolbar

--- src/App/Debug/TrackerDebugger.php
+++ src/App/Debug/TrackerDebugger.php
@@ -246,6 +246,8 @@ class TrackerDebugger implements LoggerAwareInterface
 	 */
 	public function getOutput()
 	{
+		$navigation = $this->getNavigation();
+
 		$debug = array();
 
 		// Check if debug is only displayed for admin users
@@ -285,13 +287,16 @@ class TrackerDebugger implements LoggerAwareInterface
 			$debug[] = $this->renderLanguageFiles();
 		}
 
+		return implode("\n", $navigation) . implode("\n", $debug);
+	}
+
+	private function getNavigation()
+	{
 		$navigation = array();
 
-		if ($debug)
-		{
-			// OK, here comes some very beautiful CSS !!
-			// It's kinda "hidden" here, so evil template designers won't find it :P
-			$navigation[] = '
+		// OK, here comes some very beautiful CSS !!
+		// It's kinda "hidden" here, so evil template designers won't find it :P
+		$navigation[] = '
 		<style>
 			pre.dbQuery { background-color: #333; color: white; font-weight: bold; }
 			span.dbgTable { color: yellow; }
@@ -302,35 +307,74 @@ class TrackerDebugger implements LoggerAwareInterface
 		</style>
 		';
 
-			$navigation[] = '<div class="navbar navbar-fixed-bottom" id="debugBar">';
-			$navigation[] = '<div class="navbar-inner">';
-			$navigation[] = '<a class="brand" href="javascript:;">Debug</a>';
-			$navigation[] = '<ul class="nav">';
+		$navigation[] = '<div class="navbar navbar-fixed-bottom" id="debugBar">';
+		$navigation[] = '<div class="navbar-inner">';
+		$navigation[] = '<a class="brand" href="#top">&nbsp;<i class="icon icon-joomla"></i></a>';
+		$navigation[] = '<ul class="nav">';
 
-			if ($this->application->get('debug.database'))
-			{
-				$navigation[] = '<li><a href="#dbgDatabase">Database</a></li>';
-			}
-
-			if ($this->application->get('debug.system'))
-			{
-				$navigation[] = '<li><a href="#dbgProfile">Profile</a></li>';
-				$navigation[] = '<li><a href="#dbgUser">User</a></li>';
-				$navigation[] = '<li><a href="#dbgProject">Project</a></li>';
-			}
-
-			if ($this->application->get('debug.language'))
-			{
-				$navigation[] = '<li><a href="#dbgLanguageStrings">Lang Strings</a></li>';
-				$navigation[] = '<li><a href="#dbgLanguageFiles">Lang Files</a></li>';
-			}
-
-			$navigation[] = '</ul>';
-			$navigation[] = '</div>';
-			$navigation[] = '</div>';
+		if ($this->application->get('debug.database'))
+		{
+			$navigation[] = '<li><a href="#dbgDatabase"><i class="icon icon-database"></i> '
+				. $this->getBadge(count($this->getLog('db')))
+				. '</a></li>';
 		}
 
-		return implode("\n", $navigation) . implode("\n", $debug);
+		if ($this->application->get('debug.system'))
+		{
+			$profile = $this->getProfile();
+			$user    = $this->application->getSession()->get('user');
+
+			$navigation[] = '<li><a href="#dbgProfile"><i class="icon icon-lightning"></i> '
+				. sprintf('%s MB', $this->getBadge(number_format($profile->peak / 1000000, 3)))
+				. ' '
+				. sprintf('%s sec.', $this->getBadge(number_format($profile->time, 3)))
+				. '</a></li>';
+
+			$navigation[] = '<li><a href="#dbgUser"><i class="icon icon-user"></i> <span class="badge">'
+				. ($user && $user->username ? $user->username : g11n3t('Guest'))
+				.'</span></a></li>';
+
+			$navigation[] = '<li><a href="#dbgProject"><i class="icon icon-cube"></i> <span class="badge">'
+				. ($this->application->getSession()->get('project')->title ? : g11n3t('No Project'))
+				.'</span></a></li>';
+		}
+
+		if ($this->application->get('debug.language'))
+		{
+			$info = $this->getLanguageStringsInfo();
+			$badge = $this->getBadge($info->untranslateds, array(1 => 'badge-warning'));
+
+			$navigation[] = '<li><a href="#dbgLanguageStrings"><i class="icon icon-question-sign"></i>  ' . $badge . '/' . $this->getBadge($info->total) . '</a></li>';
+			$navigation[] = '<li><a href="#dbgLanguageFiles"><i class="icon icon-file-word"></i> ' . $this->getBadge(count(g11n::getEvents())) . '</a></li>';
+		}
+
+		$navigation[] = '</ul>';
+		$navigation[] = '</div>';
+		$navigation[] = '</div>';
+
+		return $navigation;
+	}
+
+	/**
+	 * Render the profiler output.
+	 *
+	 * @return  \stdClass
+	 *
+	 * @since   1.0
+	 */
+	private function getProfile()
+	{
+		$points = $this->profiler->getPoints();
+
+		$pointStart = $points[0]->getName();
+		$pointEnd   = $points[count($points) - 1]->getName();
+
+		$profile = new \stdClass;
+
+		$profile->peak = $this->profiler->getMemoryBytesBetween($pointStart, $pointEnd);
+		$profile->time = $this->profiler->getTimeBetween($pointStart, $pointEnd);
+
+		return $profile;
 	}
 
 	/**
@@ -389,10 +433,7 @@ class TrackerDebugger implements LoggerAwareInterface
 			. 'Previous: ' . get_class($exception->getPrevious());
 		}
 
-		$viewClass = '\\JTracker\\View\\TrackerDefaultView';
-
-		/* @type \JTracker\View\TrackerDefaultView $view */
-		$view = new $viewClass;
+		$view = new \JTracker\View\TrackerDefaultView;
 
 		$message = '';
 
@@ -617,5 +658,58 @@ class TrackerDebugger implements LoggerAwareInterface
 		$html[] = '</table>';
 
 		return implode("\n", $html);
+	}
+
+	/**
+	 * Get info about processed language strings.
+	 *
+	 * @return \stdClass
+	 *
+	 * @since  1.0
+	 */
+	protected function getLanguageStringsInfo()
+	{
+		$items = g11n::get('processedItems');
+
+		$info = new \stdClass;
+
+		$info->total = count($items);
+		$info->untranslateds = 0;
+
+		foreach ($items as $item)
+		{
+			if ('-' == $item->status)
+			{
+				$info->untranslateds ++;
+			}
+		}
+
+		return $info;
+	}
+
+	/**
+	 * Create a bootstrap HTML badge.
+	 *
+	 * an array with optional css class can be supplied. If the $count value exceeds the option value this class will be used.
+	 * E.g.: [5 => 'warning', 15 => 'danger']
+	 *
+	 * @param   integer  $count  The number to display inside the badge.
+	 * @param   array    $options  An indexed array of values and CSS classes.
+	 *
+	 * @return string
+	 */
+	private function getBadge($count, array $options = array())
+	{
+		$class = '';
+
+		foreach ($options as $opCount => $opClass)
+		{
+			if ($count >= $opCount)
+			{
+				$class = ' ' . $opClass;
+			}
+		}
+
+		return '<span class="badge' . $class . '">' . $count . '</span>';
 	}
 }
