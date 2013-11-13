ae5efb74327bed6f0a0cc41971de2a252a3e090b
Michael Babker <mbabker@flbab.com>
2013-11-13 07:25:37 -0600

    Add build to comment hook for injecting issues

--- src/App/Tracker/Controller/Hooks/ReceiveCommentsHook.php
+++ src/App/Tracker/Controller/Hooks/ReceiveCommentsHook.php
@@ -179,6 +179,7 @@ class ReceiveCommentsHook extends AbstractHookController
 		$data['opened_by']       = $this->hookData->issue->user->login;
 		$data['modified_date']   = $modified->format($dateFormat);
 		$data['project_id']      = $this->project->project_id;
+		$data['build']           = $this->hookData->repository->default_branch;
 
 		// Add the closed date if the status is closed
 		if ($this->hookData->issue->closed_at)
