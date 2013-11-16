1dc7110d05cf4e96b6a0fae7bb81e84982069290
Nikolai Plath <der.el.kuku@gmail.com>
2013-11-16 15:54:02 -0500

    Remove obsolete `@package` tags

--- cli/CliApp/Command/Update/Update.php
+++ cli/CliApp/Command/Update/Update.php
@@ -2,9 +2,6 @@
 /**
  * Part of the Joomla! Tracker application.
  *
- * @package     JTracker
- * @subpackage  CLI
- *
  * @copyright   Copyright (C) 2012 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
