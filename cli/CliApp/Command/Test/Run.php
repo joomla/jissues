34c073d5f25c20b7561a748e1f63e366fe28daaf
Nikolai Plath <der.el.kuku@gmail.com>
2013-12-03 12:17:30 -0500

    Add `test run` command to run a whole "test suite"

---
+++ cli/CliApp/Command/Test/Run.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Part of the Joomla! Tracker application.
+ *
+ * @copyright  Copyright (C) 2013 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace CliApp\Command\Test;
+
+/**
+ * Class for running a test suite.
+ *
+ * @since  1.0
+ */
+class Run extends Test
+{
+	/**
+	 * The command "description" used for help texts.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	protected $description = 'Run all tests';
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function execute()
+	{
+		$this->application->outputTitle('Test Suite');
+
+		with(new Checkstyle)->execute();
+
+		with(new Phpunit)->execute();
+
+		$this
+			->out()
+			->out('Finished =;)');
+	}
+}
