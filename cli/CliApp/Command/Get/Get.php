2c114be2b5cdf37066f5a8aa70b043be04e9f8c1
Nikolai Plath <der.el.kuku@gmail.com>
2013-12-13 09:57:25 -0500

    Add check and switch to the get command

--- cli/CliApp/Command/Get/Get.php
+++ cli/CliApp/Command/Get/Get.php
@@ -13,9 +13,9 @@ use App\Projects\Table\ProjectsTable;
 use CliApp\Command\TrackerCommand;
 use CliApp\Command\TrackerCommandOption;
 
-use Joomla\Github\Github;
 use Joomla\Filesystem\Folder;
 
+use JTracker\Github\Github;
 use JTracker\Container;
 
 /**
@@ -32,6 +32,14 @@ class Get extends TrackerCommand
 	 * @since  1.0
 	 */
 	protected $github;
+
+	/**
+	 * The id of the current bot.
+	 *
+	 * @var    integer
+	 * @since  1.0
+	 */
+	protected $botId = 0;
 
 	/**
 	 * Project object.
@@ -57,12 +65,6 @@ class Get extends TrackerCommand
 				new TrackerCommandOption(
 					'project', 'p',
 					'Process the project with the given ID.'
-				)
-			)
-			->addOption(
-				new TrackerCommandOption(
-					'auth', '',
-					'Use GitHub credentials from configuration for authentication.'
 				)
 			)
 			->addOption(
@@ -115,6 +117,75 @@ class Get extends TrackerCommand
 	protected function setupGitHub()
 	{
 		$this->github = Container::retrieve('gitHub');
+
+		return $this;
+	}
+
+	/**
+	 * Check the remaining GitHub rate limit.
+	 *
+	 * @param   integer  $remaining  The remaining count.
+	 *
+	 * @throws \RuntimeException
+	 * @return $this
+	 *
+	 * @since   1.0
+	 */
+	protected function checkGitHubRateLimit($remaining)
+	{
+		// @todo hard coded values..
+		$minSwitch = 100;
+		$minRemain = 10;
+
+		$this->debugOut(sprintf('Limit check: %1$d -- %2$d / %3$d', $remaining, $minSwitch, $minRemain));
+
+		if ($remaining <= $minSwitch)
+		{
+			$this->switchGitHubAccount();
+		}
+
+		if ($remaining <= $minRemain)
+		{
+			throw new \RuntimeException(
+				sprintf(
+					'GitHub remaining rate limit (%1$d) dropped below the minimum (%2$d) for user %3$s.',
+					$remaining, $minRemain, $this->github->getOption('api.username')
+				)
+			);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Cycle through a list of GitHub accounts for "long running processes".
+	 *
+	 * @return $this
+	 *
+	 * @since   1.0
+	 * @throws \UnexpectedValueException
+	 */
+	public function switchGitHubAccount()
+	{
+		$accounts = $this->github->getOption('api.accounts');
+
+		if (!$accounts)
+		{
+			throw new \UnexpectedValueException('No GitHub accounts set in config.');
+		}
+
+		// Increase or reset the bot id counter.
+		$this->botId = ($this->botId + 1 >= count($accounts))
+			? 0
+			: $this->botId + 1;
+
+		$username = $accounts[$this->botId]->username;
+		$password = $accounts[$this->botId]->password;
+
+		$this->github->setOption('api.username', $username);
+		$this->github->setOption('api.password', $password);
+
+		$this->logOut(sprintf('Switched to bot account %s (%d)', $username, $this->botId));
 
 		return $this;
 	}
