bf0eef748a98a2e61cf6221be8d137b282bb5138
Michael Babker <mbabker@flbab.com>
2014-03-01 13:21:12 -0600

    Add support for labeling CMS pulls that have JC trackers

--- src/App/Tracker/Controller/Hooks/Listeners/JoomlacmsPullsListener.php
+++ src/App/Tracker/Controller/Hooks/Listeners/JoomlacmsPullsListener.php
@@ -8,6 +8,8 @@
 
 namespace App\Tracker\Controller\Hooks\Listeners;
 
+use App\Tracker\Table\IssuesTable;
+
 use Joomla\Event\Event;
 use Joomla\Github\Github;
 
@@ -37,8 +39,8 @@ class JoomlacmsPullsListener
 		// Only perform these events if this is a new pull, action will be 'opened'
 		if ($arguments['action'] === 'opened')
 		{
-			// Check pull requests for a PR-<branch> label
-			$this->checkPullLabel($arguments['hookData'], $arguments['github'], $arguments['logger'], $arguments['project']);
+			// Check that pull requests have certain labels
+			$this->checkPullLabel($arguments['hookData'], $arguments['github'], $arguments['logger'], $arguments['project'], $arguments['table']);
 
 			// Check if the pull request targets the master branch
 			$this->checkMasterBranch($arguments['hookData'], $arguments['github'], $arguments['logger'], $arguments['project']);
@@ -105,20 +107,22 @@ class JoomlacmsPullsListener
 	/**
 	 * Checks for a PR-<branch> label
 	 *
-	 * @param   object  $hookData  Hook data payload
-	 * @param   Github  $github    Github object
-	 * @param   Logger  $logger    Logger object
-	 * @param   object  $project   Object containing project data
+	 * @param   object       $hookData  Hook data payload
+	 * @param   Github       $github    Github object
+	 * @param   Logger       $logger    Logger object
+	 * @param   object       $project   Object containing project data
+	 * @param   IssuesTable  $table     Table object
 	 *
 	 * @return  void
 	 *
 	 * @since   1.0
 	 */
-	protected function checkPullLabel($hookData, Github $github, Logger $logger, $project)
+	protected function checkPullLabel($hookData, Github $github, Logger $logger, $project, IssuesTable $table)
 	{
 		// Set some data
 		$issueLabel = 'PR-' . $hookData->pull_request->base->ref;
-		$labelSet   = false;
+		$addLabels  = array();
+		$prLabelSet = false;
 
 		// Get the labels for the pull's issue
 		try
@@ -140,12 +144,12 @@ class JoomlacmsPullsListener
 			return;
 		}
 
-		// Check if the PR- label present
+		// Check if the PR- label present if there are already labels attached to the item
 		if (count($labels) > 0)
 		{
 			foreach ($labels as $label)
 			{
-				if (!$labelSet && $label->name == $issueLabel)
+				if (!$prLabelSet && $label->name == $issueLabel)
 				{
 					$logger->info(
 						sprintf(
@@ -157,44 +161,58 @@ class JoomlacmsPullsListener
 						)
 					);
 
-					$labelSet = true;
+					$prLabelSet = true;
 				}
 			}
 		}
 
-		// Add the label if we need to
-		if (!$labelSet)
+		// Add the issueLabel if it isn't already set
+		if (!$prLabelSet)
 		{
-			try
-			{
-				$github->issues->labels->add(
-					$project->gh_user, $project->gh_project, $hookData->pull_request->number, array($issueLabel)
-				);
+			$addLabels[] = $issueLabel;
+		}
 
-				// Post the new label on the object
-				$logger->info(
-					sprintf(
-						'Added %s label to %s/%s #%d',
-						$issueLabel,
-						$project->gh_user,
-						$project->gh_project,
-						$hookData->pull_request->number
-					)
-				);
-			}
-			catch (\DomainException $e)
-			{
-				$logger->error(
-					sprintf(
-						'Error adding the %s label to GitHub pull request %s/%s #%d - %s',
-						$issueLabel,
-						$project->gh_user,
-						$project->gh_project,
-						$hookData->pull_request->number,
-						$e->getMessage()
-					)
-				);
-			}
+		/*
+		 * If we have a foreign ID in the IssuesTable object, then there is a JoomlaCode tracker
+		 * NOTE: If someone ever changes these labels on GitHub, this has to be changed
+		 */
+		if (isset($table->foreign_number))
+		{
+			$addLabels[] = 'Has JoomlaCode Tracker Item';
+		}
+		else
+		{
+			$addLabels[] = 'Needs JoomlaCode Tracker Item';
+		}
+
+		try
+		{
+			$github->issues->labels->add(
+				$project->gh_user, $project->gh_project, $hookData->pull_request->number, $addLabels
+			);
+
+			// Post the new label on the object
+			$logger->info(
+				sprintf(
+					'Added %s labels to %s/%s #%d',
+					count($addLabels),
+					$project->gh_user,
+					$project->gh_project,
+					$hookData->pull_request->number
+				)
+			);
+		}
+		catch (\DomainException $e)
+		{
+			$logger->error(
+				sprintf(
+					'Error adding labels to GitHub pull request %s/%s #%d - %s',
+					$project->gh_user,
+					$project->gh_project,
+					$hookData->pull_request->number,
+					$e->getMessage()
+				)
+			);
 		}
 	}
 }
