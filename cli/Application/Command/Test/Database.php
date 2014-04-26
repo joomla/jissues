0d182585d7df3ddc072a15ee934e1a2286331719
Nikolai Plath <der.el.kuku@gmail.com>
2014-04-26 15:04:26 -0500

    Add database info command `test database`

---
+++ cli/Application/Command/Test/Database.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Part of the Joomla! Tracker application.
+ *
+ * @copyright  Copyright (C) 2012 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
+
+namespace Application\Command\Test;
+
+use Symfony\Component\Console\Input\ArrayInput;
+
+/**
+ * Class for testing the database.
+ *
+ * @since  1.0
+ */
+class Database extends Test
+{
+	/**
+	 * The command "description" used for help texts.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	protected $description = 'Get information about database entities.';
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return  $this
+	 *
+	 * @since   1.0
+	 */
+	public function execute()
+	{
+		$this->getApplication()->outputTitle('Drop Database');
+
+		return $this->getContainer()
+			->get('DoctrineRunner')
+			->run(new ArrayInput(['command' => 'orm:info']));
+	}
+}
