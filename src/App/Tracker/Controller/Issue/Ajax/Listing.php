7d992244f2110aa5b055c5f3715eda6daaab9967
Allen Zhao <cnallenzhao@gmail.com>
2014-05-19 23:57:27 +0800

    Adding filtering and sort for Ajax Listing

--- src/App/Tracker/Controller/Issue/Ajax/Listing.php
+++ src/App/Tracker/Controller/Issue/Ajax/Listing.php
@@ -44,8 +44,65 @@ class Listing extends AbstractAjaxController
 		$state->set('filter.project', $projectId);
 		//set state
 		$this->model->setState($state);
+
+		$sort = $application->getUserStateFromRequest('project_' . $projectId . '.filter.sort', 'filter-sort', 0, 'uint');
+
+		switch ($sort)
+		{
+			case 1:
+				$state->set('list.ordering', 'a.issue_number');
+				$state->set('list.direction', 'ASC');
+				break;
+
+			case 2:
+				$state->set('list.ordering', 'a.modified_date');
+				$state->set('list.direction', 'DESC');
+				break;
+
+			case 3:
+				$state->set('list.ordering', 'a.modified_date');
+				$state->set('list.direction', 'ASC');
+				break;
+
+			default:
+				$state->set('list.ordering', 'a.issue_number');
+				$state->set('list.direction', 'DESC');
+		}
+
+		$state->set('filter.sort', $sort);
+
+		$state->set('filter.priority',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.priority', 'filter-priority', 0, 'uint')
+		);
+
+		$state->set('filter.status',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.status', 'filter-status', 0, 'uint')
+		);
+
+		$state->set('filter.stage',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.stage', 'filter-stage', 0, 'uint')
+		);
+
+		$state->set('filter.search',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.search', 'filter-search', '', 'string')
+		);
+
+		$state->set('filter.user',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.user', 'filter-user', 0, 'uint')
+		);
+
+		$state->set('stools-active',
+			$application->input->get('stools-active', 0, 'uint')
+		);
+
+		if ($application->getUser()->username)
+		{
+			$state->set('username', $application->getUser()->username);
+		}
 		//send response.
-		$this->response->data = $this->model->getItems();
+		$listItems=$this->model->getItems();
+
+		$this->response->data = $listItems;
 	}
 
 } 
