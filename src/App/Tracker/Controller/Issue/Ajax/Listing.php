e0047b94be2fc6c7e999763fb7800ee38b1f57d0
Allen Zhao <cnallenzhao@gmail.com>
2014-06-25 23:14:11 +0800

    Change the request methods into GET
    Implement the controller part.

--- src/App/Tracker/Controller/Issue/Ajax/Listing.php
+++ src/App/Tracker/Controller/Issue/Ajax/Listing.php
@@ -28,6 +28,95 @@ class Listing extends AbstractAjaxController
 	protected $model;
 
 	/**
+	 * Setting model state that will be used for filtering.
+	 *
+	 * @param   \JTracker\Application      $application  The application object
+	 * @param   \Joomla\Registry\Registry  $state        The state object
+	 *
+	 * @return \Joomla\Registry\Registry
+	 */
+	private function setModelState(\JTracker\Application $application, \Joomla\Registry\Registry $state)
+	{
+		// Pagination
+
+		$limit = $application->getUserStateFromRequest('list.limit', 'limit', 20, 'int');
+		$page  = $application->input->getInt('page');
+
+		$value      = $page ? ($page - 1) * $limit : 0;
+		$limitStart = ($limit != 0 ? (floor($value / $limit) * $limit) : 0);
+
+		$state->set('list.start', $limitStart);
+		$state->set('list.limit', $limit);
+
+		// Get project id;
+
+		$projectId = $application->getProject()->project_id;
+
+		// Set filter of project
+		$state->set('filter.project', $projectId);
+
+		// Get sort and direction
+		$this->model->setState($state);
+
+		$sort = $application->getUserStateFromRequest('project_' . $projectId . '.filter.sort', 'sort', 'issue_number', 'word');
+
+		$direction = $application->getUserStateFromRequest('project_' . $projectId . '.filter.direction', 'direction', 'desc', 'word');
+
+		switch (strtolower($sort))
+		{
+			case 'modified_date':
+				$state->set('list.ordering', 'a.modified_date');
+				break;
+
+			default:
+				$state->set('list.ordering', 'a.issue_number');
+		}
+
+		switch (strtoupper($direction))
+		{
+			case 'ASC':
+				$state->set('list.direction', 'ASC');
+				break;
+
+			default:
+				$state->set('list.direction', 'DESC');
+		}
+
+		$state->set('filter.sort', $sort);
+
+		$state->set('filter.priority',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.priority', 'priority', 0, 'uint')
+		);
+
+		$state->set('filter.status',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.status', 'status', 0, 'uint')
+		);
+
+		$state->set('filter.stage',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.stage', 'stage', 0, 'uint')
+		);
+
+		$state->set('filter.search',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.search', 'search', '', 'string')
+		);
+
+		$state->set('filter.user',
+			$application->getUserStateFromRequest('project_' . $projectId . '.filter.user', 'user', 0, 'uint')
+		);
+
+		$state->set('stools-active',
+			$application->input->get('stools-active', 0, 'uint')
+		);
+
+		if ($application->getUser()->username)
+		{
+			$state->set('username', $application->getUser()->username);
+		}
+
+		return $state;
+	}
+
+	/**
 	 * Prepare the response.
 	 *
 	 * @return  void
@@ -51,88 +140,17 @@ class Listing extends AbstractAjaxController
 		// Get state object
 		$state = $this->model->getState();
 
-		// Pagination
-		$limit = $application->getUserStateFromRequest('list.limit', 'list_limit', 20, 'int');
-		$page  = $this->getContainer()->get('app')->input->getInt('page');
-
-		$value      = $page ? ($page - 1) * $limit : 0;
-		$limitStart = ($limit != 0 ? (floor($value / $limit) * $limit) : 0);
-
-		$state->set('list.start', $limitStart);
-		$state->set('list.limit', $limit);
-
-		// Get project id;
-
-		$projectId = $application->getProject()->project_id;
-
-		// Set filter of project
-		$state->set('filter.project', $projectId);
-
-		// Set state
-		$this->model->setState($state);
-
-		$sort = $application->getUserStateFromRequest('project_' . $projectId . '.filter.sort', 'filter-sort', 0, 'uint');
-
-		switch ($sort)
-		{
-			case 1:
-				$state->set('list.ordering', 'a.issue_number');
-				$state->set('list.direction', 'ASC');
-				break;
-
-			case 2:
-				$state->set('list.ordering', 'a.modified_date');
-				$state->set('list.direction', 'DESC');
-				break;
-
-			case 3:
-				$state->set('list.ordering', 'a.modified_date');
-				$state->set('list.direction', 'ASC');
-				break;
-
-			default:
-				$state->set('list.ordering', 'a.issue_number');
-				$state->set('list.direction', 'DESC');
-		}
-
-		$state->set('filter.sort', $sort);
-
-		$state->set('filter.priority',
-			$application->getUserStateFromRequest('project_' . $projectId . '.filter.priority', 'filter-priority', 0, 'uint')
-		);
-
-		$state->set('filter.status',
-			$application->getUserStateFromRequest('project_' . $projectId . '.filter.status', 'filter-status', 0, 'uint')
-		);
-
-		$state->set('filter.stage',
-			$application->getUserStateFromRequest('project_' . $projectId . '.filter.stage', 'filter-stage', 0, 'uint')
-		);
-
-		$state->set('filter.search',
-			$application->getUserStateFromRequest('project_' . $projectId . '.filter.search', 'filter-search', '', 'string')
-		);
-
-		$state->set('filter.user',
-			$application->getUserStateFromRequest('project_' . $projectId . '.filter.user', 'filter-user', 0, 'uint')
-		);
-
-		$state->set('stools-active',
-			$application->input->get('stools-active', 0, 'uint')
-		);
-
-		if ($application->getUser()->username)
-		{
-			$state->set('username', $application->getUser()->username);
-		}
+		// Set model state
+		$this->model->setState($this->setModelState($application, $state));
 
 		// Pagination
 		$paginationObject = new TrackerPagination(new Uri($this->getContainer()->get('app')->get('uri.request')));
-
 		$this->model->setPagination($paginationObject);
 
+		// Get list items
 		$listItems = $this->model->getAjaxItems();
 
+		// Get total pages
 		$pagesTotal = $this->model->getPagination()->getPagesTotal();
 
 		// Render the label html for each item
@@ -145,7 +163,6 @@ class Listing extends AbstractAjaxController
 
 		// Prepare the response.
 		$items                = array('items' => $listItems, 'pagesTotal' => $pagesTotal);
-
 		$this->response->data = $items;
 	}
 }
