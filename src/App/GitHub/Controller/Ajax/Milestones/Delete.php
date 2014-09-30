fd4180691ec561ed8895e136d5dac2a7151c2846
Nikolai Plath <der.el.kuku@gmail.com>
2014-09-30 09:41:52 -0500

    Add more milestone management

---
+++ src/App/GitHub/Controller/Ajax/Milestones/Delete.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Part of the Joomla Tracker's GitHub Application
+ *
+ * @copyright  Copyright (C) 2014 Open Source Matters, Inc. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
+
+namespace App\GitHub\Controller\Ajax\Milestones;
+
+use JTracker\Github\GithubFactory;
+
+/**
+ * Controller class to delete milestones from the GitHub repository.
+ *
+ * @since  1.0
+ */
+class Delete extends Base
+{
+	/**
+	 * Prepare the response.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function prepareResponse()
+	{
+		/* @type \JTracker\Application $application */
+		$application = $this->getContainer()->get('app');
+
+		$application->getUser()->authorize('manage');
+
+		$milestoneId = $application->input->getUint('milestone_id');
+
+		$project = $application->getProject();
+
+		// Look if we have a bot user configured.
+		if ($project->getGh_Editbot_User() && $project->getGh_Editbot_Pass())
+		{
+			$gitHub = GithubFactory::getInstance($application, true, $project->getGh_Editbot_User(), $project->getGh_Editbot_Pass());
+		}
+		else
+		{
+			$gitHub = GithubFactory::getInstance($application);
+		}
+
+		// Delete the label
+		$gitHub->issues->milestones->delete($project->gh_user, $project->gh_project, $milestoneId);
+
+		// Get the current labels list.
+		$this->response->data = $this->getList($project);
+	}
+}
--- src/App/GitHub/Controller/Ajax/Milestones/GetList.php
+++ src/App/GitHub/Controller/Ajax/Milestones/GetList.php
@@ -2,20 +2,18 @@
 /**
  * Part of the Joomla Tracker's GitHub Application
  *
- * @copyright  Copyright (C) 2012 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2014 Open Source Matters, Inc. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
 namespace App\GitHub\Controller\Ajax\Milestones;
-
-use JTracker\Controller\AbstractAjaxController;
 
 /**
  * Controller class to display milestones on the GitHub repository.
  *
  * @since  1.0
  */
-class GetList extends AbstractAjaxController
+class GetList extends Base
 {
 	/**
 	 * Prepare the response.
@@ -28,37 +26,6 @@ class GetList extends AbstractAjaxController
 	{
 		$this->getContainer()->get('app')->getUser()->authorize('manage');
 
-		$project = $this->getContainer()->get('app')->getProject();
-
-		/* @type \Joomla\Github\Github $github */
-		$gitHub = $this->getContainer()->get('gitHub');
-
-		$data = $gitHub->issues->milestones->getList($project->gh_user, $project->gh_project);
-
-		$milestones = [];
-
-		foreach ($data as $item)
-		{
-			// This is to keep request data short..
-
-			$milestone = new \stdClass;
-
-			$milestone->number      = $item->number;
-			$milestone->title       = $item->title;
-			$milestone->state       = $item->state;
-			$milestone->description = $item->description;
-			$milestone->due_on      = $item->due_on;
-
-			$milestones[] = $milestone;
-		}
-
-		usort(
-			$milestones, function ($a, $b)
-			{
-				return $a->number > $b->number;
-			}
-		);
-
-		$this->response->data = $milestones;
+		$this->response->data = $this->getList($this->getContainer()->get('app')->getProject());
 	}
 }
