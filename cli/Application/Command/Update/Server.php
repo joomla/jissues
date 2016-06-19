681ab39dfa559de58b91bfdb93a61edd958af27e
Michael Babker <mbabker@flbab.com>
2016-06-19 17:35:32 -0500

    Tweaks on 'update server' command

--- cli/Application/Command/Update/Server.php
+++ cli/Application/Command/Update/Server.php
@@ -8,6 +8,7 @@
 
 namespace Application\Command\Update;
 
+use Application\Command\Database\Migrate;
 use Application\Command\Make\Repoinfo;
 use Application\Command\TrackerCommandOption;
 use Application\Exception\AbortException;
@@ -20,20 +21,16 @@ use Application\Exception\AbortException;
 class Server extends Update
 {
 	/**
-	 * The command "description" used for help texts.
-	 *
-	 * @var    string
-	 * @since  1.0
-	 */
-	protected $description = 'Updates the local installation to either a specified version or latest HEAD for the active branch';
-
-	/**
 	 * Constructor.
 	 *
 	 * @since   1.0
 	 */
 	public function __construct()
 	{
+		parent::__construct();
+
+		$this->description = g11n3t('Updates the local installation to either a specified version or latest git HEAD for the active branch');
+
 		$this->addOption(
 			new TrackerCommandOption(
 				'version', 'v',
@@ -64,63 +61,25 @@ class Server extends Update
 			// Fetch from remote sources and checkout the specified version tag
 			$this->execCommand('cd ' . JPATH_ROOT . ' && git fetch && git checkout ' . $version . ' 2>&1');
 
-			// Update the Composer installation
-			$this->execCommand('cd ' . JPATH_ROOT . ' && composer install --no-dev --optimize-autoloader 2>&1');
-
-			/*
-			 * If a SQL diff exists for the version requested, then process it.  It is assumed that the person
-			 * updating will not be skipping versions with this implementation, but if need be, that can be supported.
-			 */
-			$sqlFile = JPATH_CONFIGURATION . '/updates/' . $version . '.sql';
-
-			if (file_exists($sqlFile))
-			{
-				$queries = file_get_contents($sqlFile);
-
-				if ($queries === false)
-				{
-					throw new AbortException(
-						sprintf(
-							g11n3t('Could not read data from the %s SQL file, please update the database manually.'),
-							$sqlFile
-						)
-					);
-				}
-
-				/** @var \Joomla\Database\DatabaseDriver $db */
-				$db = $this->getContainer()->get('db');
-
-				$dbQueries = $db->splitSql($queries);
-
-				foreach ($dbQueries as $query)
-				{
-					try
-					{
-						$db->setQuery($query)->execute();
-					}
-					catch (\RuntimeException $e)
-					{
-						throw new AbortException(
-							sprintf(
-								g11n3t(
-									'SQL query failed, please verify the database structure and finish the update '
-									. 'manually.  The database error message is: %s'),
-								$e->getMessage()
-							)
-						);
-					}
-				}
-			}
-
-			$this->logOut(sprintf('Update to version %s successful', $version));
+			$message = sprintf('Update to version %s successful', $version);
 		}
 		else
 		{
 			// Perform a git pull on the active branch
 			$this->execCommand('cd ' . JPATH_ROOT . ' && git pull 2>&1');
 
-			$this->logOut('Git update Finished');
+			$message ='Git update Finished';
 		}
+
+		// Update the Composer installation
+		$this->execCommand('cd ' . JPATH_ROOT . ' && composer install --no-dev --optimize-autoloader 2>&1');
+
+		// Execute the database migrations (if any) for this version
+		(new Migrate)
+			->setContainer($this->getContainer())
+			->execute();
+
+		$this->logOut($message);
 
 		(new Repoinfo)
 			->setContainer($this->getContainer())
