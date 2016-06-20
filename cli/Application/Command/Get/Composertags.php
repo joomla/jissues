bd76e1dadcf791b036c7dd8dbe1d5e4ebcea104b
Nikolai Plath <der.el.kuku@gmail.com>
2016-06-20 10:36:33 -0500

    Test if comments for translators are supported by Crowdin

--- cli/Application/Command/Get/Composertags.php
+++ cli/Application/Command/Get/Composertags.php
@@ -53,6 +53,7 @@ class Composertags extends Get
 
 		if (!$packages)
 		{
+			// TRANSLATORS: %s refers to a file system path.
 			throw new \UnexpectedValueException(sprintf(g11n3t('Can not read the packages file at %s'), $path));
 		}
 
