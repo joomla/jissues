087681e82865686d38e040c13b8f7527af06e8b3
Michael Babker <mbabker@flbab.com>
2016-08-19 19:56:55 -0500

    Flush Twig cache too

--- src/App/Support/Controller/Clearcache.php
+++ src/App/Support/Controller/Clearcache.php
@@ -9,11 +9,12 @@
 namespace App\Support\Controller;
 
 use ElKuKu\G11n\Support\ExtensionHelper;
-
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
 use JTracker\Controller\AbstractTrackerController;
 
 /**
- * Controller class for the icons view.
+ * Controller class for clearing the application's cache.
  *
  * @since  1.0
  */
@@ -37,11 +38,34 @@ class Clearcache extends AbstractTrackerController
 		{
 			ExtensionHelper::cleanCache();
 
-			$application->enqueueMessage(g11n3t('The cache has been cleared.'), 'success');
+			$application->enqueueMessage(g11n3t('The translation cache has been cleared.'), 'success');
 		}
 		catch (\Exception $e)
 		{
 			$application->enqueueMessage($e->getMessage(), 'error');
+		}
+
+		// Skip Twig cache if disabled
+		if (!$application->get('renderer.cache', false))
+		{
+			$application->redirect('/');
+		}
+
+		$cacheDir     = JPATH_ROOT . '/cache';
+		$twigCacheDir = $application->get('renderer.cache');
+
+		$filesystem = new Filesystem(new Local($cacheDir));
+
+		if ($filesystem->has($twigCacheDir))
+		{
+			if (!$filesystem->deleteDir($twigCacheDir))
+			{
+				$application->enqueueMessage(g11n3t('There was an error clearing the Twig cache.'), 'error');
+			}
+			else
+			{
+				$application->enqueueMessage(g11n3t('The Twig cache has been cleared.'), 'success');
+			}
 		}
 
 		$application->redirect('/');
