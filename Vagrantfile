30532e1069ecfd0b3e62b1ad07ee6e1472935b9c
Dmitry Rekun <bzzjuka@gmail.com>
2016-12-05 21:42:56 +0200

    Set up new vagrant box

--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,42 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "debian-7-64-elkuku-4"
-  config.vm.box_url = "https://dl.dropboxusercontent.com/s/nc9fdcl4mtgsve3/debian-7-64-elkuku-4.box"
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "jissues-debian-jessie64"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  config.vm.box_url = "https://atlas.hashicorp.com/debian/boxes/contrib-jessie64/versions/8.6.2/providers/virtualbox.box"
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.network :forwarded_port, guest: 80, host: 2345
+
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # Example for VirtualBox:
+  #
+  config.vm.provider :virtualbox do |vb|
+  #   # Don't boot with headless mode
+      vb.gui = false
+  #
+  #   # Use VBoxManage to customize the VM. For example to change memory:
+  #   vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+  #
+  # View the documentation for the provider you're using for more
+  # information on available options.
+
+  config.vm.provision "shell", inline: "apt-get install --yes puppet"
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "build/puppet/manifests"
+  # puppet.manifest_file  = "site.pp"
   end
-  config.vm.network :forwarded_port, host: 2345, guest: 80
-  config.vm.provider "virtualbox" do |v|
-    v.gui = false
-  end
+
 end
--- build/puppet/manifests/default.pp
+++ build/puppet/manifests/default.pp
@@ -1,108 +1,108 @@
 Exec {
-	path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+  path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
 
 exec { 'apt-get update':
-	path => '/usr/bin',
+  path => '/usr/bin',
 }
 
 include jissues
 
 class jissues {
-	file { '/var/www/jissues':
-		target  => '/vagrant/www',
-		ensure  => 'link',
-		require => Package['apache2'],
-		notify  => Service['apache2'],
-	}
+  file { '/var/www/jissues':
+    target  => '/vagrant/www',
+    ensure  => 'link',
+    require => Package['apache2'],
+    notify  => Service['apache2'],
+  }
 }
 
 include system
 
 class system {
-	package { ['curl', 'gettext']:
-		ensure  => installed,
-		require => Exec['apt-get update'],
-	}
-	file { '/etc/environment':
-		ensure  => present,
-		source  => '/vagrant/build/puppet/files/etc/environment',
-		owner => 'root',
-		group => 'root';
-	}
+  package { ['curl', 'gettext']:
+    ensure  => installed,
+    require => Exec['apt-get update'],
+  }
+  file { '/etc/environment':
+    ensure  => present,
+    source  => '/vagrant/build/puppet/files/etc/environment',
+    owner => 'root',
+    group => 'root';
+  }
 }
 
 include apache
 
 class apache {
-	package { 'apache2':
-		name    => 'apache2-mpm-prefork',
-		ensure  => installed,
-		require => Exec['apt-get update'],
-	}
+  package { 'apache2':
+    name    => 'apache2-mpm-prefork',
+    ensure  => installed,
+    require => Exec['apt-get update'],
+  }
 
-	service { 'apache2':
-		ensure  => running,
-		require => Package['apache2'],
-	}
+  service { 'apache2':
+    ensure  => running,
+    require => Package['apache2'],
+  }
 
-	exec { 'enable-mod_rewrite':
-		require => Package['apache2'],
-		before  => Service['apache2'],
-		command => '/usr/sbin/a2enmod rewrite'
-	}
+  exec { 'enable-mod_rewrite':
+    require => Package['apache2'],
+    before  => Service['apache2'],
+    command => '/usr/sbin/a2enmod rewrite'
+  }
 
-	file { '/etc/apache2/sites-available/jissues.virt':
-		ensure  => present,
-		require => Package['apache2'],
-		source  => '/vagrant/build/puppet/files/apache/jissues.virt';
-	}
+  file { '/etc/apache2/sites-available/jissues.conf':
+    ensure  => present,
+    require => Package['apache2'],
+    source  => '/vagrant/build/puppet/files/apache/jissues.conf';
+  }
 
-	exec { 'enable-site':
-		require => [Package['apache2'], File['/etc/apache2/sites-available/jissues.virt']],
-		before  => Service['apache2'],
-		command => 'a2ensite jissues.virt && a2dissite 000-default'
-	}
+  exec { 'enable-site':
+    require => [Package['apache2'], File['/etc/apache2/sites-available/jissues.conf']],
+    before  => Service['apache2'],
+    command => 'a2ensite jissues.conf && a2dissite 000-default'
+  }
 }
 
 include mysql
 
 class mysql {
-	package {
-		['mysql-server', 'mysql-client']:
-		ensure  => installed,
-		require => Exec['apt-get update'],
-	}
+  package {
+    ['mysql-server', 'mysql-client']:
+    ensure  => installed,
+    require => Exec['apt-get update'],
+  }
 
-	service { 'mysql':
-		ensure  => 'running',
-		require => Package['mysql-server'],
-	}
+  service { 'mysql':
+    ensure  => 'running',
+    require => Package['mysql-server'],
+  }
 }
 
 include php
 
 class php {
-	package { [
-		'php5',
-		'php5-mysql',
-		'php5-curl',
-		'php5-xdebug',
-		'php5-cli'
-	]:
-		ensure  => 'installed',
-		require => Exec['apt-get update'],
-	}
+  package { [
+    'php5',
+    'php5-mysql',
+    'php5-curl',
+    'php5-xdebug',
+    'php5-cli'
+  ]:
+    ensure  => 'installed',
+    require => Exec['apt-get update'],
+  }
 
-	file { '/etc/php5/conf.d/21-xdebug.ini':
-		ensure  => present,
-		require => Package['php5'],
-		source  => '/vagrant/build/puppet/files/php/21-xdebug.ini';
-	}
+  file { '/etc/php5/apache2/conf.d/21-xdebug.ini':
+    ensure  => present,
+    require => Package['php5'],
+    source  => '/vagrant/build/puppet/files/php/21-xdebug.ini';
+  }
 
-	file { '/etc/php5/conf.d/666-php.ini':
-		ensure  => present,
-		require => Package['php5'],
-		source  => '/vagrant/build/puppet/files/php/666-php.ini';
-	}
+  file { '/etc/php5/apache2/conf.d/99-php.ini':
+    ensure  => present,
+    require => Package['php5'],
+    source  => '/vagrant/build/puppet/files/php/99-php.ini';
+  }
 }
