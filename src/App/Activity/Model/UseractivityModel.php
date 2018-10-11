90459502943e5b1bfe56ae362342f366aea9ac69
Phil E. Taylor <phil@phil-taylor.com>
2018-10-11 12:23:51 +0100

    Only allow preset periods

--- src/App/Activity/Model/UseractivityModel.php
+++ src/App/Activity/Model/UseractivityModel.php
@@ -30,7 +30,14 @@ class UseractivityModel extends AbstractTrackerDatabaseModel
 		$query = $db->getQuery(true);
 
 		$periodList  = [1 => '-7 DAY', 2 => '-30 Day', 3 => '-90 DAY', 4 => '-1 YEAR', 5 => 'Custom'];
-		$periodValue = $periodList[$this->state->get('list.period', 1)];
+
+		$period = $this->state->get('list.period', 1);
+		
+		if (!in_array($period, array_keys($periodList))){
+			$period = 1;
+		}
+
+		$periodValue = $periodList[$period];
 
 		$typeList = ['All', 'Tracker', 'Test', 'Code'];
 		$type     = $typeList[$this->state->get('list.activity_type')];
