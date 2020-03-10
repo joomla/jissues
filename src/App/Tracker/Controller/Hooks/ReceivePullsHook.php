2832860d5c74198da06f6117a6149a8e1240dadd
George Wilson <georgejameswilson@googlemail.com>
2020-03-10 15:24:03 +0000

    Fix syntax

--- src/App/Tracker/Controller/Hooks/ReceivePullsHook.php
+++ src/App/Tracker/Controller/Hooks/ReceivePullsHook.php
@@ -128,7 +128,7 @@ class ReceivePullsHook extends AbstractHookController
 					'modified_by'     => $this->hookData->sender->login,
 					'project_id'      => $this->project->project_id,
 					'has_code'        => 1,
-					'is_draft'        => $this->data-draft ? 1 : 0,
+					'is_draft'        => $this->data->draft ? 1 : 0,
 					'build'           => $this->data->base->ref,
 					'pr_head_sha'     => $this->data->head->sha,
 					'pr_head_user'    => (isset($this->data->head->user)) ? $this->data->head->user->login : 'unknown_repository',
