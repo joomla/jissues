0f73c0b543ad687128af054ac721ec5eff9f3b24
wilsonge <georgejameswilson@googlemail.com>
2021-04-06 23:22:25 +0100

    Fix PSR-4 loading of unused class

--- src/App/Tracker/Exception/ValidationException.php
+++ src/App/Tracker/Exception/ValidationException.php
@@ -6,7 +6,7 @@
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
-namespace App\Tracker;
+namespace App\Tracker\Exception;
 
 /**
  * Class ValidationException.
