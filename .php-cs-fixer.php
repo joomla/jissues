4bb9e2b693000f9e155fbbb343597f4769055798
Hannes Papenberg <info@joomlager.de>
2024-03-18 22:45:07 +0100

    Updating Drone setup to use PSR-12

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,97 +1,75 @@
 <?php
+/**
+ * @package    Joomla.Site
+ *
+ * @copyright  (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-$mainFinder = PhpCsFixer\Finder::create()
-	->in(
-		[
-			__DIR__ . '/cli',
-			__DIR__ . '/src',
-			__DIR__ . '/tests',
-		]
-	);
+/**
+ * This is the configuration file for php-cs-fixer
+ *
+ * @link https://github.com/FriendsOfPHP/PHP-CS-Fixer
+ * @link https://mlocati.github.io/php-cs-fixer-configurator/#version:3.0
+ *
+ *
+ * If you would like to run the automated clean up, then open a command line and type one of the commands below
+ *
+ * To run a quick dry run to see the files that would be modified:
+ *
+ *        ./libraries/vendor/bin/php-cs-fixer fix --dry-run
+ *
+ * To run a full check, with automated fixing of each problem :
+ *
+ *        ./libraries/vendor/bin/php-cs-fixer fix
+ *
+ * You can run the clean up on a single file if you need to, this is faster
+ *
+ *        ./libraries/vendor/bin/php-cs-fixer fix --dry-run administrator/index.php
+ *        ./libraries/vendor/bin/php-cs-fixer fix administrator/index.php
+ */
 
-return (new PhpCsFixer\Config)
-	->setRules(
-		[
-			// psr-1
-			'encoding'                              => true,
-			'full_opening_tag'                      => true,
-			// psr-2
-			'blank_line_after_namespace'            => true,
-			'braces'                                => [
-				'allow_single_line_closure'                   => false,
-				'position_after_anonymous_constructs'         => 'next',
-				'position_after_control_structures'           => 'next',
-				'position_after_functions_and_oop_constructs' => 'next',
-			],
-			'elseif'                                => true,
-			'function_declaration'                  => true,
-			'line_ending'                           => true,
-			'constant_case'                         => true,
-			'lowercase_keywords'                    => true,
-			'method_argument_space'                 => true,
-			'no_spaces_after_function_name'         => true,
-			'no_spaces_inside_parenthesis'          => true,
-			'no_trailing_whitespace'                => true,
-			'ordered_imports'                       => true,
-			'single_blank_line_at_eof'              => true,
-			'single_import_per_statement'           => true,
-			'single_line_after_imports'             => true,
-			'switch_case_semicolon_to_colon'        => true,
-			'visibility_required'                   => true,
-			// symfony
-			'binary_operator_spaces'                => ['operators' => ['=>' => 'align', '=' => 'align']],
-			'blank_line_before_statement'           => [
-				'statements' => [
-					'break', 'case', 'continue', 'declare', 'for', 'foreach', 'if', 'return', 'switch', 'throw', 'try',
-				],
-			],
-			'cast_spaces'                           => true,
-			'concat_space'                          => ['spacing' => 'one'],
-			'dir_constant'                          => true,
-			'function_to_constant'                  => true,
-			'function_typehint_space'               => true,
-			'include'                               => true,
-			'increment_style'                       => ['style' => 'post'],
-			'is_null'                               => true,
-			'lowercase_static_reference'            => true,
-			'magic_constant_casing'                 => true,
-			'modernize_types_casting'               => true,
-			'native_function_casing'                => true,
-			'no_alias_functions'                    => true,
-			'no_blank_lines_after_class_opening'    => true,
-			'no_blank_lines_after_phpdoc'           => true,
-			'no_empty_statement'                    => true,
-			'no_extra_blank_lines'                  => true,
-			'no_trailing_comma_in_list_call'        => true,
-			'no_trailing_comma_in_singleline_array' => true,
-			'no_unneeded_control_parentheses'       => true,
-			'no_unused_imports'                     => true,
-			'no_whitespace_before_comma_in_array'   => true,
-			'no_whitespace_in_blank_line'           => true,
-			'phpdoc_trim'                           => true,
-			'return_type_declaration'               => true,
-			'self_accessor'                         => true,
-			'simplified_null_return'                => true,
-			'single_blank_line_before_namespace'    => true,
-			'single_quote'                          => true,
-			'trailing_comma_in_multiline'           => true,
-			'whitespace_after_comma_in_array'       => true,
-			'yoda_style'                            => ['equal' => false, 'identical' => false],
-			// misc
-			'array_indentation'                     => true,
-			'array_syntax'                          => ['syntax' => 'short'],
-			'combine_consecutive_issets'            => true,
-			'combine_consecutive_unsets'            => true,
-			'linebreak_after_opening_tag'           => true,
-			'logical_operators'                     => true,
-			'native_function_invocation'            => ['include' => ['@compiler_optimized']],
-			'no_null_property_initialization'       => true,
-			'no_superfluous_elseif'                 => true,
-			'no_useless_else'                       => true,
-			'no_useless_return'                     => true,
-			'ternary_to_null_coalescing'            => true,
-		]
-	)
-	->setRiskyAllowed(true)
-	->setIndent("\t")
-	->setFinder($mainFinder);
+// Add all the core Joomla folders
+$finder = PhpCsFixer\Finder::create()
+    ->in(
+        [
+            __DIR__ . '/cli',
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ]
+    );
+
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setHideProgress(false)
+    ->setUsingCache(false)
+    ->setRules(
+        [
+            // Basic ruleset is PSR 12
+            '@PSR12'                         => true,
+            // Short array syntax
+            'array_syntax'                   => ['syntax' => 'short'],
+            // List of values separated by a comma is contained on a single line should not have a trailing comma like [$foo, $bar,] = ...
+            'no_trailing_comma_in_singleline' => true,
+            // Arrays on multiline should have a trailing comma
+            'trailing_comma_in_multiline'    => ['elements' => ['arrays']],
+            // Align elements in multiline array and variable declarations on new lines below each other
+            'binary_operator_spaces'         => ['operators' => ['=>' => 'align_single_space_minimal', '=' => 'align']],
+            // The "No break" comment in switch statements
+            'no_break_comment'               => ['comment_text' => 'No break'],
+            // Remove unused imports
+            'no_unused_imports'              => true,
+            // Classes from the global namespace should not be imported
+            'global_namespace_import'        => ['import_classes' => false, 'import_constants' => false, 'import_functions' => false],
+            // Alpha order imports
+            'ordered_imports'                => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
+            // There should not be useless else cases
+            'no_useless_else'                => true,
+            // Native function invocation
+            'native_function_invocation'     => ['include' => ['@compiler_optimized']],
+        ]
+    )
+    ->setFinder($finder);
+
+return $config;
